5c153e525d621e001375334e18831f5f4ced460c
Ni But Crazy <3104955349@qq.com>
2024-12-03 22:09:39 +0800

    决定引入vue来进行游戏开发

---
+++ vue-game/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app1 {
+  margin-top: 50px;
+}
+</style>
---
+++ vue-game/vue.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin('define').tap((definitions) => {
+      Object.assign(definitions[0], {
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
+      })
+      return definitions
+    })
+  }
+})
